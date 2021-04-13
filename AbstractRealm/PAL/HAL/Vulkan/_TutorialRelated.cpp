48f15b6ba51a7e3b605875bcd980dd3c78086884
Ed94 <edwardgz@gmail.com>
2021-04-13 11:39:01 -0400

    Project should compile from pull...

--- AbstractRealm/PAL/HAL/Vulkan/_TutorialRelated.cpp
+++ AbstractRealm/PAL/HAL/Vulkan/_TutorialRelated.cpp
@@ -4,8 +4,8 @@
 #define TINYOBJLOADER_IMPLEMENTATION
 #include <tiny_obj_loader.h>
 
-//#define STB_IMAGE_IMPLEMENTATION
-//#include "stb/stb_image.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
 
 
 
