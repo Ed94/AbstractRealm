5453166315cce7c1e52dc261e1f79af7eb029c66
Ed94 <edwardgz@gmail.com>
2022-04-08 22:23:08 -0400

    Added EABase, more progress on pal.

--- Engine/Build/PrecompiledIntermediate.hpp
+++ Engine/Build/PrecompiledIntermediate.hpp
@@ -7,16 +7,26 @@
 
 #pragma region LAL
 #include "LAL.Asserts.hpp"
-#include "LAL.Bit.hpp"
+#include "LAL.Bits.hpp"
 #include "LAL.Casting.hpp"
+#include "LAL.CString.hpp"
 #include "LAL.Config.hpp"
+#include "LAL.DArray.hpp"
+#include "LAL.Data.hpp"
 #include "LAL.Declarations.hpp"
+#include "LAL.Limits.hpp"
 #include "LAL.Math.hpp"
 #include "LAL.Memory.hpp"
 #include "LAL.Memory.Allocators.hpp"
+#include "LAL.Metaprogramming.hpp"
+#include "LAL.NumericLimits.hpp"
+#include "LAL.Numerics.hpp"
 #include "LAL.Platform.hpp"
-#include "LAL.Reflection.hpp"
+#include "LAL.RefWrap.hpp"
+#include "LAL.String.hpp"
+#include "LAL.Tuple.hpp"
 #include "LAL.Types.hpp"
+#include "LAL.UTF.hpp"
 #pragma endregion LAL
 
 #pragma region OSAL
